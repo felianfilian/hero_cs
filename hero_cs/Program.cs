7e5985eca4266a072c34c5fd5ca338ccb3cda1fb
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-01 01:32:32 +0200

    Initial commit

---
+++ hero_cs/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Hero CS");
+        Console.WriteLine("HP: 10 / 10\n" + "OOOOOOOOOO");
+        
+    }
+}
