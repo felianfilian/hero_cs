81757bd7bd2652ff2d68c4567dfaed7e96a1fa64
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-01 05:20:37 +0200

    health display bugfix

--- hero_cs/Program.cs
+++ hero_cs/Program.cs
@@ -2,21 +2,21 @@
 
 public class Hero
 {
-    public int health = 10;
+    
     private static void Main(string[] args)
     {
-        //int health = 10;
-        Hero hero = new Hero(); 
+        int health = 10; 
         Console.WriteLine("Welcome to Hero CS");
-        ShowHealth(hero.health, 10);
+        ShowHealth(health, 10);
         Console.Write("damage: ");
         int damage = Convert.ToInt32(Console.ReadLine());
-        hero.health -= damage;
-        ShowHealth(hero.health, 10);
+        health -= damage;
+        ShowHealth(health, 10);
     }
 
     static void ShowHealth(int actual, int max) 
     {                   
+        
         string healthDisplay = "";
         int healthDispAmount = 10 * actual / max;
         for(int i=0; i < 10; i++) 
